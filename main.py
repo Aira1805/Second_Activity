23cfd8e755a874a755014823be87c1ddc8e4f123
AIRA <144906782+Aira1805@users.noreply.github.com>
2023-09-20 05:49:40 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+student_name = input("Student Name:")
+ID = input("ID:")
+course = input("Course:")
+section = input("Section:")
+G1 = eval(input("First Quarter Grade:"))
+G2 = eval(input("Second Quarter Grade:"))
+G3 = eval(input("Third Quarter Grade:"))
+G4 = eval(input("Fourth Quarter Grade:"))
+
+ave =(G1 + G2 + G3+ G4) / 4
+
+print(student_name)
+print(ID)
+print(course)
+print(section)
+print(G1)
+print(G2)
+print(G3)
+print(G4)
+print(ave)
